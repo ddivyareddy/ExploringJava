9036244338ca0fdebd943a4c1d271846660e403f
divyareddyd <divya.15reddy@gmail.com>
2020-06-01 19:13:13 +0530

    testing of code

---
+++ src/com/java/examples/Main.java
@@ -0,0 +1,10 @@
+package com.java.examples;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("-------------Hello welcome to java---------");
+
+    }
+}
